edc059e28729a8ba0dd186df5e3a655333555ae1
chenxinjie <>
2025-05-22 16:31:05 +0800

    更新404页面文案

--- src/views/404.vue
+++ src/views/404.vue
@@ -3,7 +3,7 @@
     <div class="content">
       <h1>404是撒懂法守法</h1>
       <h2>页面不存在</h2>
-      <p>抱歉，您访问的页面不存在或已被删除</p>
+      <p>抱歉抱歉，您访问的页面不存在或已被删除</p>
       <el-button type="primary" @click="goHome">返回首页</el-button>
     </div>
   </div>
